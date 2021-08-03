9873ef34b34902c31062722903ed284dfb4f8ef9
Kakamotobi <daeram.chung@gmail.com>
2021-08-04 03:02:54 +0900

    Store JWT In local storage

--- public/js/login.js
+++ public/js/login.js
@@ -10,18 +10,34 @@ loginForm.addEventListener("submit", async (evt) => {
 	// Make a new FormData object based on the key-value pairs of this form (input name and value).
 	const formData = new FormData(loginForm);
 
-	await fetch("http://3.34.235.190:8080/user/logIn", {
-		method: "POST",
-		body: JSON.stringify(Object.fromEntries(formData)),
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then(async (res) => {
-			const { AccessToken } = await res.json();
-			console.log(AccessToken);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	logIn(formData);
 });
+
+// --Login Function-- //
+const logIn = async (formData) => {
+	try {
+		await fetch("http://3.34.235.190:8080/user/logIn", {
+			method: "POST",
+			body: JSON.stringify(Object.fromEntries(formData)),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then(async (res) => {
+			if (res.status === 200) {
+				const { AccessToken, RefreshToken } = await res.json();
+				localStorage.setItem("jwtAccessToken", AccessToken);
+				localStorage.setItem("jwtRefreshToken", RefreshToken);
+				console.log(localStorage.getItem("jwtAccessToken"));
+				console.log(localStorage.getItem("jwtRefreshToken"));
+				// console.log(AccessToken);
+				// console.log(RefreshToken);
+			} else if (res.status === 400) {
+				console.log(res);
+			} else {
+				console.log(res);
+			}
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
