33f5f48ad22feb76894d90568568e0414b5eb684
Kakamotobi <daeram.chung@gmail.com>
2021-08-31 15:59:00 +0900

    Connect to APIs

--- public/js/makepost.js
+++ public/js/makepost.js
@@ -1,4 +1,4 @@
-// -----Editor----- //
+// -----Text Editor----- //
 const options = {
 	modules: {
 		toolbar: [
@@ -8,7 +8,7 @@ const options = {
 			[{ align: [] }],
 			[{ indent: "-1" }, { indent: "+1" }],
 			[{ list: "ordered" }, { list: "bullet" }],
-			["image", "video", "link"],
+			["image", "link"],
 		],
 	},
 	placeholder: "모임에 대한 자세한 내용을 입력해주세요",
@@ -17,57 +17,133 @@ const options = {
 
 const quill = new Quill("#editor", options);
 
+// -----Text Editor Image Handler----- //
+quill.getModule("toolbar").addHandler("image", () => {
+	imgHandler();
+});
+
+const imgHandler = () => {
+	const input = document.createElement("input");
+	input.setAttribute("type", "file");
+	input.setAttribute("accept", "image/.jpeg, .jpg, .png");
+	input.click();
+
+	input.addEventListener("change", async () => {
+		const file = input.files[0];
+		const imgFormData = new FormData();
+
+		imgFormData.append("files", file);
+
+		// Post image to API (returns image location as string)
+		await uploadImgToServer(imgFormData);
+	});
+};
+
 // -----Form Submit----- //
 const postForm = document.querySelector(".post-form");
-const thumbnail = document.querySelector("#thumbnail-input");
+const thumbnailInput = document.querySelector("#thumbnail-input");
 const editorContent = document.querySelector(".ql-editor");
 
-postForm.addEventListener("submit", (evt) => {
+postForm.addEventListener("submit", async (evt) => {
 	evt.preventDefault();
 
 	const postFormData = new FormData(postForm);
 
+	// Parse dates
 	const startAt = postFormData.get("start_at").replace(/-/g, "");
 	const finishAt = postFormData.get("finish_at").replace(/-/g, "");
 	postFormData.set("start_at", startAt);
 	postFormData.set("finish_at", finishAt);
 
+	// Editor contents
 	const content = editorContent.innerHTML;
 	postFormData.set("content", content);
 
+	// Thumbnail
 	const thumbnailData = new FormData();
-	thumbnailData.append("multipartFile", thumbnail.files[0]);
+	thumbnailData.append("multipartFile", thumbnailInput.files[0]);
 
+	// Send thumbnail to server and retrieve url from server
+	const Thumbnail_URL = await uploadThumbnailToServer(thumbnailData);
+
+	// Add Thumbnail_URL to postFormData
+	postFormData.append("thumbnail", Thumbnail_URL);
+
+	// Send final submit to server
+	uploadContentsToServer(postFormData);
+});
+
+// -----Display Thumbnail----- //
+thumbnailInput.addEventListener("change", (evt) => {
+	setThumbnail(evt);
+});
+
+// -----API Handlers----- //
+// Thumbnail API
+const uploadThumbnailToServer = async (thumbnailData) => {
 	fetch("http://3.34.235.190:8080/board/thumbnail", {
 		method: "POST",
 		headers: setHeaders({}),
 		body: thumbnailData,
-	}).then(async (res) => {
-		const data = await res.json();
-
-		const { Thumbnail_URL } = data;
-
-		postFormData.append("thumbnail", Thumbnail_URL);
-
-		fetch("http://3.34.235.190:8080/board", {
-			method: "POST",
-			headers: setHeaders({
-				"Content-Type": "application/json",
-			}),
-			body: JSON.stringify(Object.fromEntries(postFormData)),
-		}).then(async (res) => {
+	})
+		.then(async (res) => {
 			const data = await res.json();
-			console.log(data);
+			const { Thumbnail_URL } = data;
+
+			console.log("Thumbnail successfully uploaded to server");
+
+			return Thumbnail_URL;
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	});
-});
+};
 
-// -----Thumbnail----- //
-const thumbnailInput = document.querySelector("#thumbnail-input");
+// Final Submit API
+const uploadContentsToServer = (postFormData) => {
+	fetch("http://3.34.235.190:8080/board", {
+		method: "POST",
+		headers: setHeaders({
+			"Content-Type": "application/json",
+		}),
+		body: JSON.stringify(Object.fromEntries(postFormData)),
+	})
+		.then(async (res) => {
+			const data = await res.json();
+			console.log("Final submit successfully uploaded to server");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
-thumbnailInput.addEventListener("change", (evt) => {
-	setThumbnail(evt);
-});
+// Text Editor Images API
+const uploadImgToServer = async (imgFormData) => {
+	fetch("http://3.34.235.190:8080/board/images", {
+		method: "POST",
+		headers: setHeaders({}),
+		body: imgFormData,
+	})
+		.then(async (res) => {
+			const data = await res.json();
+			const img = Object.keys(data)[0];
+			const imgURL = data[img];
+
+			// Save current cursor state
+			const range = quill.getSelection(true);
+
+			// Insert image
+			quill.insertEmbed(range.index, "image", imgURL);
+
+			// Move cursor to the right side of image for easier typing
+			quill.setSelection(range.index + 1);
+
+			console.log("Image successfully uploaded to server");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 // -----Utility Functions----- //
 // Display Thumbnail
@@ -75,12 +151,13 @@ function setThumbnail(evt) {
 	const imgContainer = document.querySelector("#image-container");
 
 	const reader = new FileReader();
-	reader.onload = function (evt) {
+
+	reader.addEventListener("load", (evt) => {
 		const img = document.createElement("img");
 		img.src = evt.target.result;
 		img.classList.add("thumbnail");
 		imgContainer.appendChild(img);
-	};
+	});
 	reader.readAsDataURL(evt.target.files[0]);
 }
 
